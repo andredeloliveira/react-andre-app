f9f0b7b419210ad3614c9c00137970eff17e292f
Andre de Oliveira <oliveira.andrede@gmail.com>
2018-04-13 11:09:45 -0300

    Add link to redirect to main page

--- src/components/UI/Presentation/Presentation.jsx
+++ src/components/UI/Presentation/Presentation.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Presentation.css';
 
-const Presentation = ({name, description}) => (
-  <div className='main-container'>
-    <p className='name-container'>
-      {name}
-    </p>
-    <p className='description-container'>
-      {description}
-    </p>
-  </div>
+const Presentation = (props) => (
+  <a href='/'>
+    <div className='main-container'>
+      <p className='name-container'>
+        {props.name}
+      </p>
+      <p className='description-container'>
+        {props.description}
+      </p>
+    </div>
+  </a>
 );
 
 export default Presentation;
