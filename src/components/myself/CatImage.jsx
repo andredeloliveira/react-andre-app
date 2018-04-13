ca45c5a210f5a75f7e49e17a981a454a0f270d0a
Andre de Oliveira <oliveira.andrede@gmail.com>
2018-04-13 10:17:45 -0300

    A few adjustments for deploy

--- src/components/myself/CatImage.jsx
+++ src/components/myself/CatImage.jsx
@@ -3,7 +3,7 @@ import './cat.css';
 const CatImage = () => (
   <div className='cat-description'>
       <p>This is a lovely cat, isn't it?</p>
-      <img className='lovely-cat' src='lovely.jpg' />
+      <img className='lovely-cat' src='lovely.jpg' alt='cute kitten' />
       <p>You can click on the navigation bar on the right to know more about me or you can enjoy this extreme cuteness</p>
   </div>
 )
